d7184c4202f063be092d22c04ca6d975af497b3a
Julie Davidsen <juliedav@stud.ntnu.no>
2021-04-26 20:58:34 +0200

    changed name on file calculatorTest

--- project/src/test/java/calculator/model/CalculatorTest.java
+++ project/src/test/java/calculator/model/CalculatorTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
-public class TestCalculator {
+public class CalculatorTest {
 	
 	private static Calculator calc;
 	
